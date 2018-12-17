93ce182b48949faa79965597e0873180682cb670
GuillaumeBadikian <bg06563t@etumet105>
2018-12-17 10:41:36 +0100

    CREAT : IPC

--- init.c
+++ init.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #include "init.h"
+
+
+int file_mess;
 
 
 int main(int argc, char * argv[], char ** envp){
@@ -20,17 +20,117 @@ int main(int argc, char * argv[], char ** envp){
    * les parametres sont bons
    */
 
-  
-  
+  creer_IPC(atoi(argv[1]),atoi(argv[2]));
+  exit(0);
 }
 
 
+void creer_IPC(int argv1, int argv2){
+    key_t cle;
+    struct stat st;
+    int mem_part, semap, res_init;
+    int * tab;
+    unsigned short val_init[1]={1};
+    pid_t pid[argv1];
+    int i;
+    
+    if ((stat(FICHIER_CLE,&st) == -1) &&
+	(open(FICHIER_CLE, O_RDONLY | O_CREAT | O_EXCL, 0660) == -1)){
+      fprintf(stderr,"Pb creation fichier cle\n");
+      exit(-1);
+    }
+
+    cle = ftok(FICHIER_CLE,LETTRE_CODE);
+    if (cle==-1){
+      printf("Pb creation cle\n");
+      exit(-1);
+    }
+
+    /* On cree le SMP et on teste s'il existe deja :    */
+    mem_part = shmget(cle,sizeof(int),IPC_CREAT | IPC_EXCL | 0660);
+    if (mem_part==-1){
+	printf("Pb creation SMP ou il existe deja\n");
+	exit(-1);
+    }
+    
+    /* Attachement de la memoire partagee :          */
+    tab = shmat(mem_part,NULL,0);
+    if (tab==(int *) -1){
+	printf("Pb attachement\n");
+	/* Il faut detruire le SMP puisqu'on l'a cree : */
+	shmctl(mem_part,IPC_RMID,NULL);
+	exit(-1);
+    }
+
+       /* On cree le semaphore (meme cle) :                     */
+    semap = semget(cle,1,IPC_CREAT | IPC_EXCL | 0660);
+    if (semap==-1){
+	printf("Pb creation semaphore ou il existe deja\n");
+	/* Il faut detruire le SMP puisqu'on l'a cree : */
+	shmctl(mem_part,IPC_RMID,NULL);
+	/* Le detachement du SMP se fera a la terminaison */
+	exit(-1);
+    }
+
+
+    
+
+    /* On l'initialise :                                     */
+    res_init = semctl(semap,1,SETALL,val_init);
+    if (res_init==-1){
+	printf("Pb initialisation semaphore\n");
+	/* On detruit les IPC deje crees : */
+	semctl(semap,1,IPC_RMID,NULL);
+	shmctl(mem_part,IPC_RMID,NULL);
+	exit(-1);
+    }
+
+    file_mess = msgget(cle,IPC_CREAT | IPC_EXCL | 0660);
+    if (file_mess==-1){
+	fprintf(stderr,"Pb creation file de message\n");
+	semctl(semap,1,IPC_RMID,NULL);
+	shmctl(mem_part,IPC_RMID,NULL);
+	exit(-1);
+    }
+
+    /* creation des archivistes */
+
+    for(i = 0; i< argv1;i++){
+      pid[i] = fork();
+      if(pid[i] == -1)
+	break;
+      if(pid[i] == 0){
+	//execve
+	fprintf(stderr,"Creation des archivistes\n");
+	exit(-1);
+      }
+    }
+
+
+    /* on cree des journalistes indefiniment */
+    /*  while(1){
+      
+	}*/
+
+
+
+    
+
+    /*provisoire */
+    semctl(semap,1,IPC_RMID,NULL);
+    shmctl(mem_part,IPC_RMID,NULL);
+    msgctl(file_mess,IPC_RMID, NULL);
+    
+  }
+
+    
 void usage(char * s){
-  printf("Usage : %s <nb archiviste> <nb themes>\n",s);  
-}
+    printf("Usage : %s <nb archiviste> <nb themes>\n",s);  
+  }
 
 int arg_valid(int  argv1, int  argv2){
-  if(argv1 < 2 || argv1>100 || argv2 < 2 || argv2 >100)
-    return 0;
-  return 1; 
-}
+    if(argv1 < 2 || argv1>100 || argv2 < 2 || argv2 >100)
+      return 0;
+    return 1; 
+  }
+
--- init.h
+++ init.h
@@ -1,8 +1,29 @@
 #ifndef _init_h_
 #define _init_h_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/msg.h>
+#include <signal.h>
+
+#define FICHIER_CLE "cle.serv"
+#define LETTRE_CODE 'a'
+
+
 void usage(char * s);
 int arg_valid(int argv1, int  argv2);
+void creer_IPC(int argv1, int  argv2);
+
+
 
 
 #endif
